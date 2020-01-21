84a3822417a07e84d4ff401c57107bc3cbcde90c
julian-zatloukal <julianzatm@gmail.com>
2020-01-21 15:02:53 -0300

    Add margins to contact form, Contact button scrolls to desired position

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,13 +2,22 @@
 
 // An object literal
 var app = {
-  init: function() {
+  init: function () {
     app.mainFunction();
   },
   mainFunction: function () {
-    //alert("This is working as intended!");
+    // jQuery code
+    
+    $("#nav-item-contacto").click(function(event) {
+      event.preventDefault();
+  
+      $('html, body').animate({
+          scrollTop: $("#contact-form-whole").offset().top + $("#contact-form-whole").height() / 2
+      }, 2000);
+  });
+
   }
 };
-$(function() {
+$(function () {
   app.init();
 });
