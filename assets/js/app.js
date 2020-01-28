036d93dee3a457c6fa6781db13879b275f97421a
julian-zatloukal <julianzatm@gmail.com>
2020-01-28 09:52:23 -0300

    CORS Fix 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,60 +12,70 @@ var app = {
       e.preventDefault();
       var URL = "https://uhsj6eim16.execute-api.us-east-1.amazonaws.com/default/mailfwd";
 
-           var Namere = /[A-Za-z]{1}[A-Za-z]/;
-           if (!Namere.test($("#name-input").val())) {
-                        alert ("Name can not less than 2 char");
-               return;
-           }
-           var mobilere = /[0-9]{10}/;
-           if (!mobilere.test($("#phone-input").val())) {
-               alert ("Please enter valid mobile number");
-               return;
-           }
-           if ($("#email-input").val()=="") {
-               alert ("Please enter your email id");
-               return;
-           }
+      var Namere = /[A-Za-z]{1}[A-Za-z]/;
+      if (!Namere.test($("#name-input").val())) {
+        alert("Name can not less than 2 char");
+        return;
+      }
+      var mobilere = /[0-9]{10}/;
+      if (!mobilere.test($("#phone-input").val())) {
+        alert("Please enter valid mobile number");
+        return;
+      }
+      if ($("#email-input").val() == "") {
+        alert("Please enter your email id");
+        return;
+      }
 
-           var reeamil = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
-           if (!reeamil.test($("#email-input").val())) {
-               alert ("Please enter valid email address");
-               return;
-           }
+      var reeamil = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
+      if (!reeamil.test($("#email-input").val())) {
+        alert("Please enter valid email address");
+        return;
+      }
 
       var name = $("#name-input").val();
       var phone = $("#phone-input").val();
       var email = $("#email-input").val();
       var subject = $("#subject-input").val();
       var desc = $("#description-input").val();
-      
+
       var data = {
-         name : name,
-         phone : phone,
-         email : email,
-         subject : subject,
-         desc : desc
-       };
+        name: name,
+        phone: phone,
+        email: email,
+        subject: subject,
+        desc: desc
+      };
+
+      const response = {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "https://studiomaule.com.ar/", // Required for CORS support to work
+          "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS 
+        },
+        body: JSON.stringify(data)
+      };
 
       $.ajax({
         type: "POST",
-        url : "https://uhsj6eim16.execute-api.us-east-1.amazonaws.com/default/mailfwd",
+        url: "https://uhsj6eim16.execute-api.us-east-1.amazonaws.com/default/mailfwd",
         dataType: "json",
         crossDomain: "true",
         contentType: "application/json; charset=utf-8",
-        data: JSON.stringify(data),
+        data: response,
 
-        
+
         success: function () {
           // clear form and show a success message
           alert("Successfull");
           document.getElementById("contact-form").reset();
-      location.reload();
+          location.reload();
         },
         error: function () {
           // show an error message
           alert("UnSuccessfull");
-        }});
+        }
+      });
     }
 
     $("#nav-item-contacto").click(function (event) {
@@ -109,7 +119,7 @@ var app = {
       body.stop().animate({ scrollTop: 0 }, 2000, 'swing');
     });
 
-    $("#contact-form").submit(function(e) {
+    $("#contact-form").submit(function (e) {
       e.preventDefault();
       submitToAPI(e);
     });
