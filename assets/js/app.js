4506e2d0861f682d46a01c882acf87b1c971e865
julian-zatloukal <julianzatm@gmail.com>
2020-01-28 08:55:17 -0300

    Add connection to AWS Lambda

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,66 @@ var app = {
   mainFunction: function () {
     // jQuery code
 
+    function submitToAPI(e) {
+      e.preventDefault();
+      var URL = "https://uhsj6eim16.execute-api.us-east-1.amazonaws.com/default/mailfwd";
+
+           var Namere = /[A-Za-z]{1}[A-Za-z]/;
+           if (!Namere.test($("#name-input").val())) {
+                        alert ("Name can not less than 2 char");
+               return;
+           }
+           var mobilere = /[0-9]{10}/;
+           if (!mobilere.test($("#phone-input").val())) {
+               alert ("Please enter valid mobile number");
+               return;
+           }
+           if ($("#email-input").val()=="") {
+               alert ("Please enter your email id");
+               return;
+           }
+
+           var reeamil = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
+           if (!reeamil.test($("#email-input").val())) {
+               alert ("Please enter valid email address");
+               return;
+           }
+
+      var name = $("#name-input").val();
+      var phone = $("#phone-input").val();
+      var email = $("#email-input").val();
+      var subject = $("#subject-input").val();
+      var desc = $("#description-input").val();
+      
+      var data = {
+         name : name,
+         phone : phone,
+         email : email,
+         subject : subject,
+         desc : desc
+       };
+
+      $.ajax({
+        type: "POST",
+        url : "https://uhsj6eim16.execute-api.us-east-1.amazonaws.com/default/mailfwd",
+        dataType: "json",
+        crossDomain: "true",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(data),
+
+        
+        success: function () {
+          // clear form and show a success message
+          alert("Successfull");
+          document.getElementById("contact-form").reset();
+      location.reload();
+        },
+        error: function () {
+          // show an error message
+          alert("UnSuccessfull");
+        }});
+    }
+
     $("#nav-item-contacto").click(function (event) {
       event.preventDefault();
 
@@ -47,27 +107,6 @@ var app = {
       event.preventDefault();
       var body = $("html, body");
       body.stop().animate({ scrollTop: 0 }, 2000, 'swing');
-    });
-
-    $('#contact-form').submit(function (event) {
-      event.preventDefault();
-      alert("Processing");
-      var form_name = $('#form_name').val();
-      var form_tel = $('#form_tel').val();
-      var form_email = $('#form_email').val();
-      var form_need = $('#form_need').val();
-      var form_message = $('#form_message').val();
-
-
-      grecaptcha.ready(function () {
-        grecaptcha.execute('6Lea19EUAAAAAAfIv-yvbpA-GH4hSAEDdPG--q-C', { action: 'homepage' }).then(function (token) {
-          console.log(token);
-          document.getElementById("token").value = token;
-          $('#contact-form').prepend('<input type="hidden" name="token" value="' + token + '">');
-          $('#contact-form').prepend('<input type="hidden" name="action" value="contact_action">');
-          $('#contact-form').unbind('submit').submit();
-        });;
-      });
     });
 
 
