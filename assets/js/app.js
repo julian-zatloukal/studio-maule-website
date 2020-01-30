9bc92c08fb3629d0d73d76dfbcf434aa085865f8
julian-zatloukal <julianzatm@gmail.com>
2020-01-30 20:34:16 -0300

    CORS Fix 8

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,7 @@ var app = {
 
     function submitToAPI(e) {
       e.preventDefault();
-      var URL = "https://cors-anywhere.herokuapp.com/https://d8wgnuyah9.execute-api.us-east-1.amazonaws.com/default";
+      var URL = "https://d8wgnuyah9.execute-api.us-east-1.amazonaws.com/default";
 
       var Namere = /[A-Za-z]{1}[A-Za-z]/;
       if (!Namere.test($("#name-input").val())) {
@@ -53,7 +53,7 @@ var app = {
         beforeSend: function(request) {
           request.setRequestHeader("Access-Control-Allow-Origin", "*");
         },
-        url: "https://cors-anywhere.herokuapp.com/https://d8wgnuyah9.execute-api.us-east-1.amazonaws.com/default",
+        url: "https://d8wgnuyah9.execute-api.us-east-1.amazonaws.com/default",
         dataType: "json",
         crossDomain: "true",
         contentType: "application/json; charset=utf-8",
