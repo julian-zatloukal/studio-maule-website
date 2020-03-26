1be2e1b3474889e8c4f62dddaa2af8fb55e761a1
Julian Zatloukal <julianzatm@gmail.com>
2020-03-26 17:23:44 -0300

    Add Slick carousel and other changes

---
+++ src/js/app-slick-carousel.js
@@ -0,0 +1,34 @@
+export function initialize() {
+    $('.slick-carousel-items').slick({
+        dots: true,
+        arrows: true,
+        infinite: false,
+        accessibility: true,
+        //autoplay: true,
+        autoplaySpeed: 5000,
+        speed: 800,
+        slidesToShow: 3,
+        adaptiveHeight: true,
+        slidesToScroll: 1,
+        cssEase: 'linear',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+
+        ]
+    });
+
+    $('.slick-carousel-items').find('.slick-list').focus();
+}
---
+++ webpack.dev.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const common = require('./webpack.common');
+const merge = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = merge(common, {
+    mode: "development",
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new CopyPlugin([
+            {
+                context: './',
+                from: 'node_modules/hyphenopoly/Hyphenopoly.js',
+                to: './js/hyphenopoly/',
+                force: true,
+                flatten: true
+            },
+            {
+                context: './',
+                from: 'node_modules/hyphenopoly/Hyphenopoly_Loader.js',
+                to: './js/hyphenopoly/',
+                force: true,
+                flatten: true
+            },
+            {
+                context: './',
+                from: 'node_modules/hyphenopoly/patterns/*',
+                to: './js/hyphenopoly/patterns/',
+                force: true,
+                flatten: true
+            }
+        ]),
+        new HtmlWebpackPlugin({
+            template: "./src/template.html"
+        })
+        
+    ],
+    node: {
+        fs: 'empty',
+    },
+    optimization: {
+        runtimeChunk: 'single'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [{
+                    loader: 'style-loader'
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "sass-loader" // compiles Sass to CSS
+                }],
+                include: [/fonts/, path.resolve(__dirname, 'src', 'scss')],
+            }, {
+                test: /\.(woff|woff2|ttf|otf|eot)$/,
+                loader: 'file-loader',
+                include: [
+                    /fonts/
+                ],
+                options: {
+                    name: '[name].[hash].[ext]',
+                    outputPath: 'fonts/',
+                    esModule: false,
+                }
+            }
+        ]
+    }
+});
