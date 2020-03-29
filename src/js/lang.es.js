6770950fa1ff432f5d1e86aad401990943917ca0
Julian Zatloukal <julianzatm@gmail.com>
2020-03-29 11:13:59 -0300

    Add service card text

--- src/js/lang.es.js
+++ src/js/lang.es.js
@@ -1,33 +1,38 @@
 var lang = {
   serviceCards: {
-    viewMore: "Ver más.",
-    buttonContent: "Consultar »",
+    viewMore: 'Ver más.',
+    buttonContent: 'Consultar »',
     cards: [
       {
-        title: 'Hello1',
-        body: 'pepe prueba'
+        title: 'Traducción pública.',
+        body:
+          'La traducción de un documento de un idioma extranjero al nacional, del idioma nacional al extranjero, tiene carácter público cuando lleva la firma y el sello de un traductor matriculado en los idiomas en los que interviene, en cumplimiento con el CTPCBA.'
       },
       {
-        title: 'Hello2',
-        body: 'pepe prueba'
+        title: 'Traducción pública de actas.',
+        body:
+          'La traducción publica de Actas de nacimiento, matrimonio y defunción; Divorcios, certificado de cosa juzgada; Adopciones con cosa juzgada; Informaciones sumarias de identidad de persona; Balances; Estatutos; Poderes; Rectificaciones.'
       },
       {
-        title: 'Hello3',
-        body: 'pepe prueba'
+        title: 'Armado de carpeta para ciudadanía.',
+        body:
+          'El pasaporte italiano concede el derecho de entrada a 187 países y territorios; siendo uno de los pasaportes más valorados. La ciudadanía italiana se basa en el principio del ius sanguinis, derecho de sangre, que sostiene que es italiano el hijo nacido de padre italiano o madre italiana.'
       },
       {
-        title: 'Hello4',
-        body: 'pepe prueba'
+        title: 'Declaración de valor de títulos.',
+        body:
+          'La declaración de valor es un documento expedido por las autoridades consulares italianas a quienes, habiendo conseguido un título de estudio en instrucción extranjeras, desean proseguir los estudios en Italia o bien iniciar los procedimientos de equiparación de Títulos de Estudio o bien de Reconocimiento profesional.'
       },
       {
-        title: 'Hello5',
-        body: 'pepe prueba'
+        title: 'Traducción no pública.',
+        body:
+          'La traducción es la actividad que consiste en comprender el significado de un texto en un idioma, llamado texto origen, para producir un texto con significado equivalente, en otro idioma, llamado texto traducido.'
       },
       {
-        title: 'Hello6',
-        body: 'pepe prueba'
-      },
-      
+        title: 'Otros.',
+        body:
+          'En caso de precisar un servicio personalizado, especifico o de consultoría.'
+      }
     ]
   },
 
