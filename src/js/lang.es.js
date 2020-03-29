f6d37c05d250a8607da5f7baf7e9b73b25ac457a
Julian Zatloukal <julianzatm@gmail.com>
2020-03-28 22:56:02 -0300

    Redesign service cards

--- src/js/lang.es.js
+++ src/js/lang.es.js
@@ -1,46 +1,76 @@
 var lang = {
-    contactForm: {
-        nameInput: {
-            completeThisField: "Por favor, complete este campo.",
-            badFormat: "Por favor, introduzca un nombre válido.",
-            tooLong: "Por favor, introduzca un nombre más corto."
-        },
-        phoneInput: {
-            completeThisField: "Por favor, complete este campo.",
-            badFormat: "Por favor, introduzca un número válido.",
-            tooLong: "Por favor, introduzca un número más corto."
-        },
-        emailInput: {
-            completeThisField: "Por favor, complete este campo.",
-            badFormat: "Por favor, introduzca un email válido.",
-            tooLong: "Por favor, introduzca un número más corto."
-        },
-        subjectInput: {
-            completeThisField: "Por favor, seleccione una opción.",
-            badFormat: "Por favor, introduzca una opción valida.",
-            tooLong: "Por favor, introduzca una opción más corta."
-        },
-        descriptionInput: {
-            completeThisField: "Por favor, complete este campo.",
-            badFormat: "Por favor, introduzca una descripción válida.",
-            tooLong: "Por favor, introduzca una descripción más corta."
-        },
-        recaptchaInput: {
-            completeThisField: "Por favor, complete esta casilla.",
-            badFormat: "Se ha producido un error.",
-            tooLong: "Se ha producido un error."
-        },
-        submitSuccess: {
-            heading: "¡Enviado!",
-            body: "Su mensaje a sido enviado y será respondido a la brevedad."
-        },
-        submitFailure: {
-            body: "<strong>Ooops!</strong> Ha ocurrido un error, intente comunicarse mediante nuestro correo electrónico."
-        }
+  serviceCards: {
+    viewMore: "Ver más.",
+    buttonContent: "Consultar »",
+    cards: [
+      {
+        title: 'Hello1',
+        body: 'pepe prueba'
+      },
+      {
+        title: 'Hello2',
+        body: 'pepe prueba'
+      },
+      {
+        title: 'Hello3',
+        body: 'pepe prueba'
+      },
+      {
+        title: 'Hello4',
+        body: 'pepe prueba'
+      },
+      {
+        title: 'Hello5',
+        body: 'pepe prueba'
+      },
+      {
+        title: 'Hello6',
+        body: 'pepe prueba'
+      },
+      
+    ]
+  },
 
+  contactForm: {
+    nameInput: {
+      completeThisField: 'Por favor, complete este campo.',
+      badFormat: 'Por favor, introduzca un nombre válido.',
+      tooLong: 'Por favor, introduzca un nombre más corto.'
+    },
+    phoneInput: {
+      completeThisField: 'Por favor, complete este campo.',
+      badFormat: 'Por favor, introduzca un número válido.',
+      tooLong: 'Por favor, introduzca un número más corto.'
+    },
+    emailInput: {
+      completeThisField: 'Por favor, complete este campo.',
+      badFormat: 'Por favor, introduzca un email válido.',
+      tooLong: 'Por favor, introduzca un número más corto.'
+    },
+    subjectInput: {
+      completeThisField: 'Por favor, seleccione una opción.',
+      badFormat: 'Por favor, introduzca una opción valida.',
+      tooLong: 'Por favor, introduzca una opción más corta.'
+    },
+    descriptionInput: {
+      completeThisField: 'Por favor, complete este campo.',
+      badFormat: 'Por favor, introduzca una descripción válida.',
+      tooLong: 'Por favor, introduzca una descripción más corta.'
+    },
+    recaptchaInput: {
+      completeThisField: 'Por favor, complete esta casilla.',
+      badFormat: 'Se ha producido un error.',
+      tooLong: 'Se ha producido un error.'
+    },
+    submitSuccess: {
+      heading: '¡Enviado!',
+      body: 'Su mensaje a sido enviado y será respondido a la brevedad.'
+    },
+    submitFailure: {
+      body:
+        '<strong>Ooops!</strong> Ha ocurrido un error, intente comunicarse mediante nuestro correo electrónico.'
     }
-
+  }
 };
-
 
 window.lang = lang;
