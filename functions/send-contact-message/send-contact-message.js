252b0a9d61c0bca5cc1bffbd4c6a415958da21c8
Julián Zatloukal Maule <julianzatm@gmail.com>
2021-03-23 17:28:12 -0300

    [testing] Change serverless function

--- functions/send-contact-message/send-contact-message.js
+++ functions/send-contact-message/send-contact-message.js
@@ -4,10 +4,8 @@ const handler = async (event, context) => {
     const subject = event.queryStringParameters.name || 'World'
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: JSON.stringify({ event: JSON.stringify(event), context: JSON.stringify(context) }),
+
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
